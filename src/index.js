308d6bf9c9a397c2e67cdf4cbb070c3486f0bad0
MBulti <>
2021-01-18 20:56:25 +0100

    install pwa

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-if ("serviceworker" in navigator) {
+if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("sw.js").then(registration => {
         console.log("ServiceWorker registered");
         console.log(registration)
@@ -6,4 +6,7 @@ if ("serviceworker" in navigator) {
         console.log("ServiceWorker failed.");
         console.log(error);
     })
+}
+else {
+  console.log("ServiceWorker not supported");
 }
